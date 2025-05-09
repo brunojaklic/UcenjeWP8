82e9d5698809467f402a81a2f326aedfa4ce0fce
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-09 18:06:23 +0200

    Kreiranje tablice smjer

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -1,3 +1,14 @@
 ﻿--create database edunovawp8;
 
 --drop database edunovawp8;
+
+--use edunovawp8;
+
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2),
+datumpokretanja datetime,
+aktivan bit
+);
+
