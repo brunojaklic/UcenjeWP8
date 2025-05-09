7f2c2f1e05b66996620731b6f167198fdf9bbbc0
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-09 19:32:40 +0200

    Kreiranje SQL skripte na osnovu ERA dijagrama muzeja

--- muzej.sql
+++ muzej.sql
@@ -1,1 +1,27 @@
-﻿
+﻿--create database muzej;
+--use muzej;
+
+--create table kostosi(
+--sifra int,
+--ime varchar(20),
+--prezime varchar(20)
+--);
+
+--create table djela(
+--sifra int,
+--naziv int
+--);
+
+--create table sponzori(
+--sifra int,
+--naziv int
+--);
+
+--create table izlozbe(
+--sifra int,
+--naziv varchar(50),
+--starost int,
+--kustos int,
+--sponzor int
+--);
+
