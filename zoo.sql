c186abccbf39a2474e489ccde61d23518245dcb6
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-09 19:41:33 +0200

    Kreiranje SQL skripte na osnovu ERA dijagrama zoo

---
+++ zoo.sql
@@ -0,0 +1,25 @@
+﻿--create database zoo;
+--use zoo;
+
+--create table zivotinje(
+--sifra int,
+--vrsta varchar(50),
+--ime varchar(20)
+--);
+
+--create table osobe(
+--sifra int,
+--ime varchar(20),
+--prezime varchar(20)
+--);
+
+--create table prostor(
+--sifra int,
+--adresa varchar(100),
+--ime int
+--);
+
+--create table sticenici(
+--zivotinja int
+--);
+
