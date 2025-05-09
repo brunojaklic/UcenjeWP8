e2231a7f6bd0bf19da3dd1e2dde24c058016b01c
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-09 18:30:39 +0200

    Kreiranje tablica

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -4,11 +4,27 @@
 
 --use edunovawp8;
 
-create table smjerovi(
-sifra int,
-naziv varchar(50),
-cijena decimal(18,2),
-datumpokretanja datetime,
-aktivan bit
-);
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--datumpokretanja datetime,
+--aktivan bit
+--);
+
+--create table grupe(
+--sifra int,
+--naziv varchar(20),
+--smjer int,
+--predavac varchar(50)
+--);
+
+--create table polaznici(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--email varchar(100),
+--);
+
+
 
