42b630c105ce8f87903c5e5c5bf722bf171305b4
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-09 19:37:39 +0200

    Kreiranje SQL skripte na osnovu ERA dijagrama vrtica

---
+++ vrtic.sql
@@ -0,0 +1,26 @@
+﻿--create database vrtic;
+--use vrtic;
+
+--create table odgajateljice(
+--sifra int,
+--ime varchar(20),
+--prezime varchar(20)
+--);
+
+--create table strucne_spreme(
+--sifta int,
+--naziv varchar(50)
+--);
+
+--create table skupine(
+--sifra int,
+--ime varchar(20),
+--odgajateljica int
+--);
+
+--create table djeca(
+--sifra int,
+--ime varchar(20),
+--prezime varchar(20),
+--skupine int
+--);
