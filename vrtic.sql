7deb3aac4cd83aa3fda078a2cd31cea09cbea7dc
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-13 19:54:13 +0200

    Puna implementacija ERA dijagrama vrtic

--- vrtic.sql
+++ vrtic.sql
@@ -1,26 +1,32 @@
-﻿--create database vrtic;
---use vrtic;
+﻿use master;
+go
+drop database if exists vrtic;
+go
+create database vrtic;
+go
+use vrtic;
 
---create table odgajateljice(
---sifra int,
---ime varchar(20),
---prezime varchar(20)
---);
+create table strucne_spreme(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null
+);
 
---create table strucne_spreme(
---sifta int,
---naziv varchar(50)
---);
+create table odgajateljice(
+sifra int not null primary key identity(1,1),
+ime varchar(20) not null,
+prezime varchar(20) not null,
+strucna_sprema int references strucne_spreme(sifra)
+);
 
---create table skupine(
---sifra int,
---ime varchar(20),
---odgajateljica int
---);
+create table skupine(
+sifra int not null primary key identity(1,1),
+ime varchar(20) not null,
+odgajateljica int references odgajateljice(sifra)
+);
 
---create table djeca(
---sifra int,
---ime varchar(20),
---prezime varchar(20),
---skupine int
---);
+create table djeca(
+sifra int not null primary key identity(1,1),
+ime varchar(20) not null,
+prezime varchar(20) not null,
+skupina int references skupine(sifra)
+);
