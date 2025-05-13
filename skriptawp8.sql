125f7d4c1dc6bf84233038ef7fd3db98194b68a0
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-13 18:43:17 +0200

    2. lauf

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -1,35 +1,38 @@
-﻿--create database edunovawp8;
+﻿
+use master;
+go
+drop database if exists edunovawp8;
+go
+create database edunovawp8;
+go
+use edunovawp8;
 
---drop database edunovawp8;
+create table smjerovi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+cijena decimal(18,2) null, -- null se ne mora pisati
+datumpokretanja datetime, -- i ovo je null iako ne piše
+aktivan bit not null default 0 
+);
 
---use edunovawp8;
+create table grupe(
+sifra int not null primary key identity(1,1),
+naziv varchar(20) not null,
+smjer int not null references smjerovi(sifra),
+predavac varchar(50)
+);
 
---create table smjerovi(
---sifra int,
---naziv varchar(50),
---cijena decimal(18,2),
---datumpokretanja datetime,
---aktivan bit
---);
+create table polaznici(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+email varchar(100),
+);
 
---create table grupe(
---sifra int,
---naziv varchar(20),
---smjer int,
---predavac varchar(50)
---);
-
---create table polaznici(
---sifra int,
---ime varchar(50),
---prezime varchar(50),
---email varchar(100),
---);
-
---create table clanovi(
---grupa int,
---polaznik int
---);
+create table clanovi(
+grupa int not null references grupe(sifra),
+polaznik int not null references polaznici(sifra)
+);
 
 
 
