a0e5e24b5a8cccbd3ef6376172afdc6db358b073
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-16 19:56:49 +0200

    Update i delete naredbe

---
+++ update_delete.sql
@@ -0,0 +1,34 @@
+﻿use edunovawp8;
+
+select * from smjerovi where sifra = 2;
+
+update smjerovi
+set aktivan = 1
+where sifra = 2;
+
+update smjerovi set 
+cijena = 1000,
+datumpokretanja = '2024-02-29'
+where sifra = 3;
+
+update smjerovi set cijena = 1000 where cijena is null;
+
+-- dižemo cijenu svih smjerova za 12%
+
+update smjerovi set cijena = cijena * 1.12;
+
+-- smanjiti cijene za 10 %
+
+update smjerovi set cijena = cijena * 0.9;
+
+-- popust na sve smjerove 10 %
+
+update smjerovi set cijena = cijena * 0.9;
+
+-- delete naredba brise podatke iz tablice
+
+delete smjerovi where sifra = 1;
+delete grupe where smjer = 1;
+delete clanovi where grupa in (select sifra from grupe where smjer = 1);
+
+-- postoji opcija cascade ALI JU NE BIH PREPORUČIO
