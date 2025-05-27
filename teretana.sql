ad7cac1b1c509dc2f183662345f7a33180e43ac8
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-05-27 13:05:00 +0200

    Puna implementacija ERA dijagrama teretana

---
+++ teretana.sql
@@ -0,0 +1,40 @@
+﻿use master;
+go
+drop database if exists teretana;
+go
+create database teretana collate Croatian_CI_AS;
+go
+use teretana;
+
+create table programi(
+	sifra int not null primary key identity(1,1),
+	naziv varchar(50) not null,
+	cijena decimal(18,2),
+	aktivan bit not null
+);
+
+create table grupe(
+	sifra int not null primary key identity(1,1),
+	naziv varchar(50) not null,
+	program int not null references programi(sifra),
+	trener varchar(50) not null
+);
+
+create table kategorije(
+	sifra int not null primary key identity(1,1),
+	naziv varchar(50) not null,
+	cijena decimal(18,2)
+);
+
+create table vjezbaci(
+	sifra int not null primary key identity(1,1),
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	email varchar(50),
+	kategorija int not null references kategorije(sifra)
+);
+
+create table clanovi(
+	grupa int not null references grupe(sifra),
+	vjezbac int not null references vjezbaci(sifra),
+);
