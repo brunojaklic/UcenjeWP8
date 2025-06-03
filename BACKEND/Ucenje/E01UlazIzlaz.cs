50f47214f2630733f7c8b68f78c834671273fd49
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-06-03 19:52:51 +0200

    Ulaz izlaz

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+        public static void Izvedi()
+        {
+
+
+            Console.Write("Pozdrav svijetu");
+            
+            Console.WriteLine("Isti red");
+
+            Console.WriteLine("Osijek je \n SUPER");
+
+            Console.WriteLine("\t\tOvo je uvuceno");
+
+            Console.Write("Unesi svoje ime: ");
+
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste {0}", ime);
+
+
+
+
+        } // kraj metode
+
+
+
+
+    } // kraj klase
+} // kraj imenskog prostora (namespace)
+
