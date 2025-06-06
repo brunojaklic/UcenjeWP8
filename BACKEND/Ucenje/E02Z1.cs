f3496f3c03c5a330eb6da1ebddba017467b0bebe
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-06-06 19:51:19 +0200

    Zadatak

---
+++ BACKEND/Ucenje/E02Z1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E02Z1
+    {
+        public static void Izvedi()
+        {
+
+
+            Console.Write("Unesi iznos place: ");
+            double placa = double.Parse(Console.ReadLine());
+            Console.WriteLine(placa);
+
+
+
+
+
+        }
+    }
+}
