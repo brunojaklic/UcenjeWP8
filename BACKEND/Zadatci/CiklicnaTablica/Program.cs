dc62ba1c3d0e83411f4765f8ecd740cfd89ea56f
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-06-12 20:24:59 +0200

    Prvi pokusaj ciklicne tablice
    Radi samo kad je m * n paran broj

---
+++ BACKEND/Zadatci/CiklicnaTablica/Program.cs
@@ -0,0 +1,3 @@
+﻿using CiklicnaTablica;
+
+Zadatak01.Izvedi();
---
+++ BACKEND/Zadatci/CiklicnaTablica/Zadatak01.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiklicnaTablica
+{
+    internal class Zadatak01
+    {
+
+        public static void Izvedi()
+        {
+            Console.Write("Unesite broj redova: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Unesite duljinu stupaca: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrica = new int[m, n];
+
+            int broj = 1;
+            int x = 0;
+
+            while (broj <= m * n)
+            {
+                for (int j = n - 1 - x; j >= (0 + x); j--)
+                {
+                    matrica[m - 1 - x, j] = broj++;
+                }
+
+                for (int i = m - 2 - x; i >= (0 + x); i--)
+                {
+                    matrica[i, x] = broj++;
+                }
+
+                for (int j = 1 + x; j <= (n - 1 - x); j++)
+                {
+                    matrica[x, j] = broj++;
+                }
+
+                for (int i = 1 + x; i <= (m - 2 - x); i++)
+                {
+                    matrica[i, n - 1 - x] = broj++;
+                }
+                x++;
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("\t{0}", matrica[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+
+
+        }
+    }
+}
