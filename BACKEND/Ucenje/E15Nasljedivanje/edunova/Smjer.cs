a385609b1138df8bd5831de74a50a599fe4ba67a
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-06-20 18:32:20 +0200

    Update Smjer.cs

--- BACKEND/Ucenje/E15Nasljedivanje/edunova/Smjer.cs
+++ BACKEND/Ucenje/E15Nasljedivanje/edunova/Smjer.cs
@@ -2,10 +2,9 @@
 
 namespace Ucenje.E15Nasljedivanje.edunova
 {
-    public class Smjer : Entitet
+    public class Smjer : SvojstvoNaziv
     {
 
-        public string Naziv { get; set; } = "";
         public decimal? Cijena { get; set; }
         public DateTime? DatumPokretanja { get; set; }
         public bool Aktivan { get; set; }
